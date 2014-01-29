ccb1e0c6cdfc2db7810f8fbc1d78513a1b23e71a
Unai Trecet <unai.trecet@gmail.com>
2014-01-29 20:38:38 +0100

    Sluggable gem added.

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'voteable_unai'
+require 'sluggable_unai'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
